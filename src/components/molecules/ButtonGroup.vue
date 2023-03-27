c165e9218deb866bfcd9b2ae5a7afaa16a0c6783
Yuri <yuri.araujo-ext@tuibrasil.com>
2023-03-26 23:25:10 -0300

    feat: add ButtonGroup component

---
+++ src/components/molecules/ButtonGroup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="button-group">
+    <ButtonAtom
+      v-for="(label, index) in list"
+      :key="`${index}-${label}`"
+      :label="label"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import ButtonAtom from '../atoms/ButtonAtom.vue';
+
+export default Vue.extend({
+  components: {
+    ButtonAtom,
+  },
+  props: {
+    list: Array,
+  },
+});
+</script>
+
+<style scoped></style>
